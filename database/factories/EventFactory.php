e0ec701841ae4f8ce07c2cd753ef9ec6a9d65b35
Matheus <matheuspsilva29@gmail.com>
2021-04-27 14:43:22 -0300

    first commit

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class EventFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Event::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence();
+        return [
+            'title' => $title,
+            'description' => $this->faker->sentence(),
+            'body' => $this->faker->paragraph(),
+            'start_event' => now(),
+            'slug' => Str::slug($title)
+        ];
+    }
+}
