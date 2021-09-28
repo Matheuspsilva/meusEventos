55507f704d2aca0741068875358499dbc8c50349
Matheus <matheus.silva@jn2.com.br>
2021-09-27 21:49:55 -0300

    add create_photos migration

---
+++ database/migrations/2021_09_28_003006_create_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Event;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Event::class,'event_id')->constrained()->cascadeOnDelete();
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
