97fa7b51140852c0ed9b3e3a9b9a260f39084ad2
Matheus <matheuspsilva29@gmail.com>
2021-11-19 16:18:36 -0300

    Add Laravel Sluggable

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,15 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
     protected $fillable = ['title', 'description', 'body', 'slug', 'start_event', 'banner'];
 
     protected $dates = ['start_event'];
+
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 
     //Accessors
 
@@ -22,12 +35,12 @@ class Event extends Model
     }
 
     //Mutators
-    public function setTitleAttribute($value){
+    // public function setTitleAttribute($value){
 
-        $this->attributes['title'] = $value;
+    //     $this->attributes['title'] = $value;
 
-        $this->attributes['slug'] = Str::slug($value);
-    }
+    //     $this->attributes['slug'] = Str::slug($value);
+    // }
 
     public function setStartEventAttribute($value){
         $this->attributes['start_event'] = (\DateTime::createFromFormat('d/m/Y H:i', $value))->format('Y-m-d H:i');
