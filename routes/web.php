6441f9cea8cb6c365168181cdfaf630308608561
Matheus <matheuspsilva29@gmail.com>
2021-11-24 09:55:35 -0300

    Account verification

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,9 @@ use App\Http\Controllers\Admin\EventPhotoController;
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\HomeController;
-use App\Models\Event;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +24,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('eventos/{event:slug}', [HomeController::class, 'show'])->name('events.single');
 
-Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
     Route::resource('events', EventController::class);
     Route::resource('events.photos', EventPhotoController::class)
         ->only(['index', 'store', 'destroy']);
@@ -37,10 +38,26 @@ Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
 
 Route::prefix('/enrollment')->name('enrollment.')->group(function(){
     Route::get('/start/{event:slug}', [EnrollmentController::class, 'start'])->name('start');
-    Route::get('/confirm', [EnrollmentController::class, 'confirm'])->name('confirm')->middleware('auth');
-    Route::get('/process', [EnrollmentController::class, 'process'])->name('process')->middleware('auth');
+    Route::get('/confirm', [EnrollmentController::class, 'confirm'])->name('confirm')->middleware(['auth', 'verified']);
+    Route::get('/process', [EnrollmentController::class, 'process'])->name('process')->middleware(['auth', 'verified']);
 
 });
+
+Route::get('/email/verify', function () {
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
 Auth::routes();
